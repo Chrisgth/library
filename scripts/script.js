8db6a59d6091238e906d4408026cf1c0d72890e5
chrisgth <ksemkus00@gmail.com>
2022-04-14 22:57:48 +0300

    Edited function to include new data in DOM

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,15 @@ function Book(title, author, pages, isread){
     this.isread = isread
 }
 //construct an object and push it to a container
+const table = document.getElementById("books")
 function addBookToLibrary(title,author,pages,isread){
     myLibrary.push(new Book(title,author,pages,isread))
+    let currentBook = [title, author, pages, isread];
+    let tablerow = document.createElement('tr')
+    for(i=0; i<4; i++){
+        let tabledata = document.createElement('td')
+        tabledata.textContent = currentBook[i];
+        tablerow.appendChild(tabledata);
+    }
+    table.appendChild(tablerow)
 }
