b431d96fc83b1cad7ed8a96d2bde4dcfb4a911f6
Josh Marsh <52214569+joshuamarsh@users.noreply.github.com>
2022-04-23 22:30:44 +0100

    Fix formatting

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,32 @@
-//container
+// container
 let myLibrary = [];
-//constructor
-function Book(title, author, pages, isread){
+
+// constructor
+function Book(title, author, pages, isread) {
     this.title = title
     this.author = author
     this.pages = pages
     this.isread = isread
 }
-//create a function that displays the myLibrary object array in a table
-const table = document.getElementById("books")
-function displayBooks(){
+
+// create a function that displays the myLibrary object array in a table
+function displayBooks () {
+    const table = document.getElementById("books")
     table.innerHTML = '';
-    let thead = document.createElement('thead')
-    let thContentArray = ['Title', 'Author', 'Pages', 'Read or Not']
-    for(i=0; i<4; i++){
+    const thead = document.createElement('thead')
+    const thContentArray = ['Title', 'Author', 'Pages', 'Read or Not']
+    for ( i = 0; i < 4; i++ ) {
         let th = document.createElement('th')
         th.textContent = thContentArray[i];
         thead.appendChild(th);
     }
     table.appendChild(thead);
-    for(a=0; a<myLibrary.length; a++){
-        let currentBookNumber = myLibrary[a]
-        let tablerow = document.createElement('tr')
+    for ( a = 0; a < myLibrary.length; a++ ) {
+        const currentBookNumber = myLibrary[a]
+        const tablerow = document.createElement('tr')
         tablerow.classList.add('tablerow');
-        for(i=0; i<4; i++){
-            let currentBook = [currentBookNumber.title, currentBookNumber.author, currentBookNumber.pages, currentBookNumber.isread]
+        for ( i = 0; i < 4; i++) {
+            const currentBook = [currentBookNumber.title, currentBookNumber.author, currentBookNumber.pages, currentBookNumber.isread]
             let tabledata = document.createElement('td')
             console.log(currentBook[i]);
             tabledata.textContent = currentBook[i];
@@ -33,8 +35,9 @@ function displayBooks(){
         table.appendChild(tablerow)
     }
 }
+
 //construct an object and push it to a container
-function addBookToLibrary(title,author,pages,isread){
-    myLibrary.push(new Book(title,author,pages,isread))
+function addBookToLibrary (title, author, pages, isread) {
+    myLibrary.push(new Book(title, author, pages, isread))
     displayBooks();
 }
