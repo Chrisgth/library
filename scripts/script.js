d964d88c7bf7b62cb2ad86e486024ce30bb1f195
chrisgth <ksemkus00@gmail.com>
2022-04-24 00:21:12 +0300

    Created function that pushes data to a table

--- scripts/script.js
+++ scripts/script.js
@@ -7,16 +7,34 @@ function Book(title, author, pages, isread){
     this.pages = pages
     this.isread = isread
 }
-//construct an object and push it to a container
+//create a function that displays the myLibrary object array in a table
 const table = document.getElementById("books")
+function displayBooks(){
+    table.innerHTML = '';
+    let thead = document.createElement('thead')
+    let thContentArray = ['Title', 'Author', 'Pages', 'Read or Not']
+    for(i=0; i<4; i++){
+        let th = document.createElement('th')
+        th.textContent = thContentArray[i];
+        thead.appendChild(th);
+    }
+    table.appendChild(thead);
+    for(a=0; a<myLibrary.length; a++){
+        let currentBookNumber = myLibrary[a]
+        let tablerow = document.createElement('tr')
+        tablerow.classList.add('tablerow');
+        for(i=0; i<4; i++){
+            let currentBook = [currentBookNumber.title, currentBookNumber.author, currentBookNumber.pages, currentBookNumber.isread]
+            let tabledata = document.createElement('td')
+            console.log(currentBook[i]);
+            tabledata.textContent = currentBook[i];
+            tablerow.appendChild(tabledata);
+        }
+        table.appendChild(tablerow)
+    }
+}
+//construct an object and push it to a container
 function addBookToLibrary(title,author,pages,isread){
     myLibrary.push(new Book(title,author,pages,isread))
-    let currentBook = [title, author, pages, isread];
-    let tablerow = document.createElement('tr')
-    for(i=0; i<4; i++){
-        let tabledata = document.createElement('td')
-        tabledata.textContent = currentBook[i];
-        tablerow.appendChild(tabledata);
-    }
-    table.appendChild(tablerow)
+    displayBooks();
 }
